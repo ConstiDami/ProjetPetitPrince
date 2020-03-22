4670c0beafc9d51bb569498f6b2519c246ba6d96
ConstiDami <61546325+ConstiDami@users.noreply.github.com>
2020-03-22 17:03:46 +0100

    test

--- Prince2020/prince.py
+++ Prince2020/prince.py
@@ -1,5 +1,6 @@
 #encoding=utf-8
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
