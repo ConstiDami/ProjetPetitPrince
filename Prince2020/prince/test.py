774b093a754b57479b8a08016758dda70557b14c
Naoeltanuki <61546342+Naoeltanuki@users.noreply.github.com>
2020-04-01 16:55:12 +0200

    Update test.py

--- Prince2020/prince/test.py
+++ Prince2020/prince/test.py
@@ -1,2 +1,3 @@
 print('hello')
-#path terminal Nao C:\Users\nims\OneDrive\Documents\GitHub\ProjetPetitPrince\Prince2020 
+#path terminal Nao C:\Users\nims\OneDrive\Documents\GitHub\ProjetPetitPrince\Prince2020
+#access token GitLab : PRsc1pyUDbN1-cCcGYTz
